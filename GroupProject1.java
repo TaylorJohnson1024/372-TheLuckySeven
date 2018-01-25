1775996d41b402c60080da20c4f67aa59be2ffb0
Taylor <taylorjohnson1024@gmail.com>
2018-01-24 22:03:59 -0600

    Added Main File

---
+++ GroupProject1.java
@@ -0,0 +1,15 @@
+package GroupProject1;
+
+/**
+ *
+ * @author Taylor Johnson
+ */
+public class GroupProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+}
